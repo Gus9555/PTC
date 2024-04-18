100c829d8e89d7b26922b16c3e712c068f8a9d38
yohacrz <yohalmodaniel16@gmail.com>
2024-04-17 19:56:02 -0600

    yohalmo lo agrego

--- views/activar.php
+++ views/activar.php
@@ -1,8 +1,9 @@
 <?php
+//holaaa
 require '../funcs/conexion.php';
 require '../funcs/funcs.php';
 
-if (isset ($_GET["id"]) and isset ($_GET['val'])) {
+if (isset($_GET["id"]) and isset($_GET['val'])) {
     $idUsuario = $_GET['id'];
     $token = $_GET['val'];
 
