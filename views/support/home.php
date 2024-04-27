7fcdae9d4372a2a1feb0b4408b6f7388c461f8ef
yohacrz <yohalmodaniel16@gmail.com>
2024-04-26 21:47:08 -0600

    ULTIMO CAMBIO

--- views/support/home.php
+++ views/support/home.php
@@ -5,6 +5,8 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css" rel="stylesheet">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    
 </head>
 
 <body>
