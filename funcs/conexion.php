0bf15c7d5a058181cb17b9bc4294a44694bb19ff
yohacrz <yohalmodaniel16@gmail.com>
2024-05-03 08:58:51 -0600

    pn
    cambio en una conexion

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,5 +1,10 @@
 <?php
+<<<<<<< Updated upstream
     $mysqli = new mysqli("localhost","root","","lifeline");
+=======
+
+    $mysqli = new mysqli("localhost","root","Info2024/*-","login");
+>>>>>>> Stashed changes
     if(mysqli_connect_error()){
         echo 'ERROR', mysqli_connect_error();
         exit();
