7e07711967b15c06e84f9736f732e7f0e53cdbfc
Gus9555 <160183960+Gus9555@users.noreply.github.com>
2024-05-16 20:55:18 -0600

    sss
    ssss

--- views/index.php
+++ views/index.php
@@ -147,21 +147,19 @@ if (!empty($_POST)) {
                     <!-- Sign Up Form -->
                     <div class="form-container">
                         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                            <div class="form-group">
 
-                                <input class="form-control-input" id="inputEmailAddress" name="correo" type="email"
-                                    placeholder="Enter email address" required />
-                            </div>
                             <div class="form-group">
-
-                                <input class="form-control-input" id="inputPassword" name="password" type="password"
-                                    placeholder="Enter password" />
+                                <input class="form-control-input" id="inputEmailAddress" name="correo" type="email" placeholder="Enter email address" required />
                             </div>
+
+                            <div class="form-group">
+                               <input class="form-control-input" id="inputPassword" name="password" type="password" placeholder="Enter password" />
+                            </div>
+                            
                             <div class="form-group">
                                 <div class="custom-control custom-checkbox">
                                     <input class="custom-control-input" id="rememberPasswordCheck" type="checkbox" />
-                                    <label class="custom-control-label" for="rememberPasswordCheck">Remember
-                                        password</label>
+                                    <label class="custom-control-label" for="rememberPasswordCheck">Remember password</label>
                                 </div>
                             </div>
                             <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
